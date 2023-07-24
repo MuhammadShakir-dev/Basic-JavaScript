8bb9183d20ed473307c9d518b70e0ed0fbf8c372
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-07-24 23:13:08 +0500

    String indexes and length

---
+++ indices-lenght.js
@@ -0,0 +1,9 @@
+// Strings indices and lenght.
+// Strings are indexed in jaavscript they you index numbers to position elements.
+
+let myName = "Muhammad Shakir";
+console.log(myName[0]); // string index.
+console.log(myName[16]); //undefined.
+console.log(myName.length); // string lenght property.
+console.log(myName + " Hello folk's");
+console.log(3 +  + "3") // 6
