4920113a8a4248aa50ebf46748e8166f5fa47ef6
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-07-24 23:11:29 +0500

    Introduction to strings

---
+++ intro-string.js
@@ -0,0 +1,11 @@
+// Strings basic intro
+
+let userName = 'Muhammad Shakir'
+let userAddr = 'Karachi, Pakistan'
+let userDesignation = 'Front-end Developer'
+let userGender = 'Male'
+
+console.log('My name is : ' + userName)
+console.log('I live in : ' + userAddr)
+console.log("I'm a : " + userDesignation)
+console.log("I'm : " + userGender)
