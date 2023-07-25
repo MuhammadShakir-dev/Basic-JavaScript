9c11bf0662bec9bbfc5ea1bc592ffaf8185fdf63
Muhammad Shakir <chaudhryshakir67@gmail.com>
2023-07-25 16:18:34 +0500

    include string method in js

---
+++ include().js
@@ -0,0 +1,6 @@
+let userInput = prompt('Enter you name :')
+if (userInput.includes('Shakir')) {
+  alert('Hello ' + userInput + 'you user id is 78738232')
+} else {
+  alert('Sorry you are not registered :(')
+}
